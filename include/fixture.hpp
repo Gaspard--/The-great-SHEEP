dcb3ee0b2ee3cb6115bae4cba37fdd45ee5732f6
Jakob Kellendonk <jakob.kellendonk@epitech.eu>
2016-06-14 18:23:01 +0200

    added renderable, fixture and entity

---
+++ include/fixture.hpp
@@ -0,0 +1,13 @@
+#ifndef FIXTURE_HPP_
+# define FIXTURE_HPP_
+
+# include "vect.hpp"
+
+class			Fixture
+{
+public:
+  double&		size;
+  Vect<2u, double>&	position;
+};
+
+#endif // FIXTURE_HPP_
---
+++ include/renderable.hpp
@@ -0,0 +1,10 @@
+#ifndef RENDERABLE_HPP_
+# define RENDERABLE_HPP_
+
+class Renderable
+{
+  Vect<2u, double&>	position;
+  Vect<2u, double&>	dimensions;
+};
+
+#endif // !RENDERABLE_HPP_
