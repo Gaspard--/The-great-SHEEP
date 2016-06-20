7620f0b8e36024ff25edef089837ce40ba01acff
Jakob Kellendonk <jakob.kellendonk@epitech.eu>
2016-06-20 15:08:17 +0200

    forgot to add object

---
+++ include/object.hpp
@@ -0,0 +1,27 @@
+#ifndef OBJECT_HPP_
+# define OBJECT_HPP_
+
+# include "entity.hpp"
+# include "vect.hpp"
+# include "fixture.hpp"
+# include "renderable.hpp"
+
+class PlayState;
+
+class Object : public Entity
+{
+protected:
+  Vect<2u, double> position;
+  Vect<2u, double> speed;
+  double size;
+  Vect<2u, double> dimensions;
+  Fixture fixture;
+  Renderable renderable;
+public:
+  Object(Vect<2u, double> position, int size, Vect<2u, double> dimensions, PlayState *playState);
+  virtual ~Object(void);
+
+  void update(void);
+};
+
+#endif // !OBJECT_HPP_
---
+++ source/object.cpp
@@ -0,0 +1,25 @@
+#include "object.hpp"
+#include "playstate.hpp"
+#include "fixture.hpp"
+#include "renderable.hpp"
+#include "logic.hpp"
+#include "entity_handler.hpp"
+
+Object::Object(Vect<2u, double> nposition, int nsize, Vect<2u, double> ndimensions, PlayState *playState)
+  : Entity(playState), position(nposition), speed(0, 0), size(nsize), dimensions(ndimensions),
+    fixture(&position, &speed, &size), renderable(&position, &dimensions, NULL)
+{
+  playState->getLogic().addFixture(&fixture);
+  playState->getDisplay().addRenderable(&renderable);
+  playState->getEntityHandler().addEntity(this);
+}
+
+void Object::update()
+{
+}
+
+Object::~Object()
+{
+  playState->getLogic().removeFixture(&fixture);
+  playState->getEntityHandler().removeEntity(this);
+}
