8190619f15ae97d796d7918d8521bbbe2c9880c8
bertrand <bertrand.buffat@epitech.eu>
2016-06-25 14:14:41 +0000

    negative chunks still not working

--- source/terrain.cpp
+++ source/terrain.cpp
@@ -1,9 +1,6 @@
-#include <cassert>
-#include "top_header.hpp"
 #include "terrain.hpp"
 #include "tile.hpp"
 #include "chunk.hpp"
-#include <cmath>
 
 using namespace std;
 
@@ -13,11 +10,6 @@ Terrain::Terrain(void) : terrainGenerator()
 
 Terrain::~Terrain(void)
 {
-  unsigned int i = 0;
-  while (i < chunks.size())
-    {
-      ++i;
-    }
   chunks.clear();
 }
 
@@ -38,12 +30,18 @@ void Terrain::orderTiles(Tile *tiles, Vect<2, int> pos)
 {
   int size = 16 * 16;
   int i = 0;
+  Vect<2, int> coeff(pos[0] < 0 ? (pos[0] + 1) * 16 : pos[0] * 16,
+		     pos[1] < 0 ? (pos[1] + 1) * 16 : pos[1] * 16);
 
   while (i < size)
     {
       tiles[i] =
-	terrainGenerator.genTile(Vect<2, int>(i % 16, i / 16)
-				 + pos * 16);
+      	terrainGenerator.genTile(Vect<2, int>(i % 16, i / 16) + coeff);
+
+      //swap coords
+      pos[0] < 0 ? tiles[i].pos[0] = -(~tiles[i].pos[0] & 15) + coeff[0] : 0;
+      pos[1] < 0 ? tiles[i].pos[1] = -(~tiles[i].pos[1] & 15) + coeff[1] : 0;
+
       ++i;
     }
 }
@@ -60,8 +58,8 @@ void Terrain::createChunk(Vect<2, int> pos)
 Tile const &Terrain::getTile(int x, int y)
 {
   int i;
-  Vect<2, int> pos(x < 0 ? -((-x - 1) >> 4) : x >> 4,
-		   y < 0 ? -((-y - 1) >> 4) : y >> 4);
+  Vect<2, int> pos(x < 0 ? -(-x >> 4) - 1 : x >> 4,
+		   y < 0 ? -(-y >> 4) - 1 : y >> 4);
 
   if ((i = getChunk(pos)) == -1)
     {
